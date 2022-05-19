883d74a0eb680d3cfb3a0646998f3c7d100fb26e
Guiseppe J. Muñoz Chanta <giusseppemch@gmail.com>
2022-05-18 23:08:00 -0500

    Agregar una nueva clase "Tour" a la Capa Entidad proveniente de la  Tabla creada en la BD_Agencia.

---
+++ CapaEntidad/Tour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class Tour
+    {
+        public int IdTour { get; set; }
+
+        public string FechaTour { get; set; }
+
+    }
+}
