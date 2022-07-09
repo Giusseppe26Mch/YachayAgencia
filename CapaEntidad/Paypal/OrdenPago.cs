dcd14c01a2f55a645e88c006dab02f2687d9ba67
Guiseppe J. Muñoz Chanta <67751829+Giusseppe26Mch@users.noreply.github.com>
2022-07-09 02:16:35 -0500

    Versionamiento nuevo - 09/07/2022

--- CapaEntidad/Paypal/OrdenPago.cs
+++ CapaEntidad/Paypal/OrdenPago.cs
@@ -9,20 +9,18 @@ namespace CapaEntidad.Paypal
     public class OrdenPago
     {
         public string intent { get; set; }
-
-        public List<PurchaseUnit> ourchase_units { get; set; }
-
+        public List<PurchaseUnit> purchase_units { get; set; }
         public ApplicationContext application_context { get; set; }
-
     }
+
 
     public class ItemTotal
     {
-        public string currency_code { get; set; } //Código de divisas
+        public string currency_code { get; set; }
         public string value { get; set; }
     }
 
-    public class Breackdown
+    public class Breakdown
     {
         public ItemTotal item_total { get; set; }
     }
@@ -31,6 +29,7 @@ namespace CapaEntidad.Paypal
     {
         public string currency_code { get; set; }
         public string value { get; set; }
+        public Breakdown breakdown { get; set; }
     }
 
     public class UnitAmount
@@ -38,34 +37,28 @@ namespace CapaEntidad.Paypal
         public string currency_code { get; set; }
         public string value { get; set; }
     }
+
     public class Item
     {
         public string name { get; set; }
-
         public string quantity { get; set; }
-
         public UnitAmount unit_amount { get; set; }
     }
 
     public class PurchaseUnit
     {
         public Amount amount { get; set; }
-
         public string description { get; set; }
-
         public List<Item> items { get; set; }
     }
+
 
     public class ApplicationContext
     {
         public string brand_name { get; set; }
-
         public string landing_page { get; set; }
         public string user_action { get; set; }
         public string return_url { get; set; }
-
         public string cancel_url { get; set; }
-
     }
-
 }
---
+++ CapaNegocio/CN_Paypal.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http; //Nos va permitir utilizar las API's
+using System.Net.Http.Headers;
+using Newtonsoft.Json; //Nos permite convertir nuestras clases en objetos Json
+using CapaEntidad.Paypal;
+namespace CapaNegocio
+{
+    public class CN_Paypal
+    {
+        private static string urlpaypal = ConfigurationManager.AppSettings["UrlPaypal"];
+        private static string clientId = ConfigurationManager.AppSettings["ClientId"];
+        private static string secret = ConfigurationManager.AppSettings["Secret"];
+
+        //Crear un método que nos permita crear una solicitud de cobro en paypal
+        public async Task<Respuesta_Paypal<RespuestaPago>>CrearSolicitud(OrdenPago orden)
+        {
+            Respuesta_Paypal<RespuestaPago> respuesta_paypal = new Respuesta_Paypal<RespuestaPago>();
+            //Creamos una solicitud HttpClient
+            using (var  client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlpaypal);
+                var authToken = Encoding.ASCII.GetBytes($"{clientId}:{secret}");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+
+                var json = JsonConvert.SerializeObject(orden);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                //Ejecutar la API
+                HttpResponseMessage response = await client.PostAsync("/v2/checkout/orders", data);
+                //Leer la respuesta
+                respuesta_paypal.Status = response.IsSuccessStatusCode;
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonRespuesta = response.Content.ReadAsStringAsync().Result;
+
+                    RespuestaPago respuestaPago = JsonConvert.DeserializeObject<RespuestaPago>(jsonRespuesta);
+
+                    respuesta_paypal.Response = respuestaPago;
+
+                }
+                return respuesta_paypal;
+
+            }
+
+        }
+
+
+        public async Task<Respuesta_Paypal<CapturaRespuesta>> AprobarPago(string token)
+        {
+            Respuesta_Paypal<CapturaRespuesta> respuesta_paypal = new Respuesta_Paypal<CapturaRespuesta>();
+            //Creamos una solicitud HttpClient
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(urlpaypal);
+                var authToken = Encoding.ASCII.GetBytes($"{clientId}:{secret}");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+
+                var data = new StringContent("{}", Encoding.UTF8, "application/json");
+
+                //Ejecutar la API agregando un parámetro: token
+                HttpResponseMessage response = await client.PostAsync($"/v2/checkout/orders/{token}/capture", data);
+                //Leer la respuesta
+                respuesta_paypal.Status = response.IsSuccessStatusCode;
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonRespuesta = response.Content.ReadAsStringAsync().Result;
+
+                    CapturaRespuesta capturaRespuesta = JsonConvert.DeserializeObject<CapturaRespuesta>(jsonRespuesta);
+
+                    respuesta_paypal.Response = capturaRespuesta;
+
+                }
+                return respuesta_paypal;
+
+            }
+
+        }
+
+
+    }
+}
